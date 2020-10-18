63ffff7abdb779dddd32580662060be6fa50d52f
Antonio Castiglione <antonio@minervawebdevelopment.com>
2020-10-18 12:12:07 -0400

    defer script

--- Themes/infinity-pro/functions.php
+++ Themes/infinity-pro/functions.php
@@ -444,3 +444,11 @@ function crunchify_print_scripts_styles() {
 	echo "</pre>";
     return $result;
 }
+
+function defer_parsing_of_js( $url ) {
+    if ( is_user_logged_in() ) return $url; //don't break WP Admin
+    if ( FALSE === strpos( $url, '.js' ) ) return $url;
+    if ( strpos( $url, 'jquery.js' ) ) return $url;
+    return str_replace( ' src', ' defer src', $url );
+}
+add_filter( 'script_loader_tag', 'defer_parsing_of_js', 10 );
